3ac8497357c3dac68e3fe906c83aa5f3126f8c7e
Harshit Doshi <harshitdoshi719@gmail.com>
2019-09-24 09:40:11 +0530

    Rewrote tests...

--- test/zo_appbar_test.dart
+++ test/zo_appbar_test.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:zo_appbar/zo_appbar.dart';
 
 void main() {
-  MaterialApp app = MaterialApp(
-    home: Scaffold(
-      appBar: ZOAppBar(8.0, 'Hello', '@', 'WORLD', Colors.white, [Color(0xFF1565C0), Color(0xFF0D47A1), Color(0xFF263238), Color(0xFF212121),], Brightness.dark, []),
-    ),
-  );
-  testWidgets('ZOAppBar Test', (WidgetTester tester) async {
-    await tester.pumpWidget(app);
-    expect(find.byType(AppBar), findsOneWidget);
+  test('Test for ZOAppBar UI', () {
+    final zoappbar = ZOAppBar(8.0, 'zo_appbar', '.', 'EXAMPLE', Colors.white, [Color(0xFF1565C0), Color(0xFF0D47A1), Color(0xFF263238), Color(0xFF212121),], Brightness.dark, []);
+    expect(zoappbar.elevation, 8.0);
+    expect(zoappbar.primaryText, 'zo_appbar');
+    expect(zoappbar.textSymbol, '.');
+    expect(zoappbar.secondaryText, 'EXAMPLE');
+    expect(zoappbar.textColor, Colors.white);
+    expect(zoappbar.appBarGradientColors, [Color(0xFF1565C0), Color(0xFF0D47A1), Color(0xFF263238), Color(0xFF212121),]);
+    expect(zoappbar.brightness, Brightness.dark);
+    expect(zoappbar.actionIconButtons, []);
   });
 }
